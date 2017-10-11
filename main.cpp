ed2181599cc39202dadd2f2bdac5e5d515ead5fc
xyee <bbxinyee@gmail.com>
2017-10-11 12:41:04 +0800

    Created main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,9 @@ using namespace std;
 
 int main(){
 cout << "Hello World";
-<<<<<<< HEAD
 cout << "Hotfix - Add few more endl";
 cout << endl << endl;
-
-
-=======
 cout << "What are you doing?";
->>>>>>> main
+
 return 0;
 }
